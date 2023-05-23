55b2ad34b0ac92eb226ea519a35f1957a9f62fe5
SulanAdam <sulan.adam19@gmail.com>
2023-05-23 14:35:28 +0200

    Kész

--- src/Employee.java
+++ src/Employee.java
@@ -1,8 +1,11 @@
+import java.time.LocalDate;
+
 public class Employee {
     Integer id;
     String name;
     String city;
     String address;
+    LocalDate birth;
     Double salary;
 
     public Employee() {
@@ -12,19 +15,22 @@ public class Employee {
             String name, 
             String city, 
             String address, 
+            LocalDate birth, 
             Double salary
         ) {
         this.name = name;
         this.city = city;
         this.address = address;
+        this.birth = birth;
         this.salary = salary;
     }
 
-    public Employee(Integer id, String name, String city, String address, Double salary) {
+    public Employee(Integer id, String name, String city, String address, LocalDate birth, Double salary) {
         this.id = id;
         this.name = name;
         this.city = city;
         this.address = address;
+        this.birth = birth;
         this.salary = salary;
     }
     
